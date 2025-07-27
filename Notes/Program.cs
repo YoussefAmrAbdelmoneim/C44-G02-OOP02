fb68197b5a5191200b0461b44f004685e31710e6
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-07-27 21:53:34 +0300

    Struct

--- Notes/Program.cs
+++ Notes/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Struct
+            //A struct is a value type in C#.
+            //It is used to encapsulate small groups of related variables
+            //Doesnt support inheritance
+            //3 Access Modifier Allowed Inside It [Public , Internal ,Private]
+            /*Compiler Will always Generate Parameterless Constructor That
+            Initialize Struct attributes with Default value unless you Defined a Parameterless Constructor */
+            #endregion
+
         }
     }
 }
