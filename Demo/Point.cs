8df6ce71f3fc614540749fe75fedf7151b7c8aab
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-07-27 19:22:32 +0300

    Struct

---
+++ Demo/Point.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal struct Point
+    {
+        #region Attributes
+
+        public int x;
+        public int y;
+
+        #endregion
+
+        #region Constructors
+        /// Is A Special Function 
+        ///    1. Named As Struct
+        ///    2. Has No Return Type 
+
+        #region Constructor Overloading 
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Point()
+        {
+            x = default;
+            y = default;
+        }
+
+        public Point(int number)
+        {
+            x = y = number;
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return $"({x} , {y})";
+        }
+        #endregion
+
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,60 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Struct 
+
+            #region Example 01
+            //Point P1;
+            //// Declare variable | Object From Type 'Point' 
+            //// CLR Will Allocate 8 Unitintialized Bytes At Stack 
+
+            //P1 = new Point();
+            //// New Is Just For Constructor Selection That Will Used To Initialize P1 Attribute 
+            //Console.WriteLine($"P1.X = {P1.x}"); // 0
+            //Console.WriteLine($"P1.Y = {P1.y}"); // 0
+
+
+            //P1 = new Point(10, 20);
+
+            //P1 = new (10, 20); // Syntax Sugar 
+            //Console.WriteLine($"P1.X = {P1.x}"); // 10
+            //Console.WriteLine($"P1.Y = {P1.y}"); // 20
+
+
+            //Console.WriteLine(P1); // (10 , 20)
+            //Console.WriteLine(P1.ToString()); // (10 , 20) 
+            #endregion
+
+            #region Example 02
+
+            //Point P1 = new Point(1 , 2);
+            //Console.WriteLine($"P1.X = {P1.x}"); // 1 
+            //Console.WriteLine($"P1.Y = {P1.y}"); // 2
+
+            //Point P2 =	new Point(10 , 20);
+            //Console.WriteLine($"P2.X = {P2.x}"); // 10 
+            //Console.WriteLine($"P2.Y = {P2.y}"); // 20
+
+            //P2 = P1;
+
+            //Console.WriteLine("After Assign P2 = P1");
+            //Console.WriteLine($"P1.X = {P1.x}"); // 1 
+            //Console.WriteLine($"P1.Y = {P1.y}"); // 2
+            //Console.WriteLine($"P2.X = {P2.x}"); // 1 
+            //Console.WriteLine($"P2.Y = {P2.y}"); // 2
+
+            //P1.x = 100;
+            //P1.y = 200;
+
+            //Console.WriteLine("After Changing P1 ");
+            //Console.WriteLine($"P1.X = {P1.x}"); // 100 
+            //Console.WriteLine($"P1.Y = {P1.y}"); // 200
+            //Console.WriteLine($"P2.X = {P2.x}"); // 1 
+            //Console.WriteLine($"P2.Y = {P2.y}"); // 2
+
+            #endregion
+
+            #endregion
         }
     }
 }
