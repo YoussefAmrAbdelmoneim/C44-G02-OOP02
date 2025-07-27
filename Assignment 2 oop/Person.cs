033599948d1a4829dab7bfe93e83b95b82852794
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-07-27 22:09:36 +0300

    Q1

---
+++ Assignment 2 oop/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace Assignment_2_oop
+{
+    internal struct Person
+    {
+        public string Name;
+        public int Age;
+
+        public override string ToString()
+        {
+            return $"Name:{Name}\nAge:{Age}";
+        }
+    }
+}
--- Assignment 2 oop/Program.cs
+++ Assignment 2 oop/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //Person[] person = new Person[3];
+            //person[0].Name = "Youssef";
+            //person[0].Age = 20;
+            //person[1].Name = "Ali";
+            //person[1].Age = 21;
+            //person[2].Name = "Mohamed";
+            //person[2].Age = 22;
+            //foreach (Person item in person)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
         }
     }
 }
