9027c9fc897a757d58bc1ca623be250c4fdf491b
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-07-27 19:17:38 +0300

    Add project files.

---
+++ Assignment 2 oop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_2_oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
