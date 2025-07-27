7aa1510b49d29aa66cd771d95262fc776ad35fe3
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-07-27 21:54:55 +0300

    Encapsulation

--- Notes/Program.cs
+++ Notes/Program.cs
@@ -12,7 +12,14 @@
             /*Compiler Will always Generate Parameterless Constructor That
             Initialize Struct attributes with Default value unless you Defined a Parameterless Constructor */
             #endregion
-
+            #region Encapsulation
+            /*
+             * It refers to the bundling of data (fields) and the methods (functions) that operate on
+             * that data into a single unit or class. It provide restricting access to some of an object's internal state (Data)
+             * and requiring all interaction to be performed through well-defined methods (Getter and Setter). 
+             */
+            //This protects the internal data and ensures that it can only be modified in a controlled way.
+            #endregion
         }
     }
 }
